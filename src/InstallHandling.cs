c94df502dcbcc2ccc8d18f445d9356e16e6d4d54
0xNim <57798165+Nim1com@users.noreply.github.com>
2023-05-13 23:22:30 -0400

    plan

--- src/InstallHandling.cs
+++ src/InstallHandling.cs
@@ -6,6 +6,26 @@ namespace ModInstaller
         {
             // Convert modID and versionNumber to versionID
             //Requests.VersionNumberToVersionID(modID, versionNumber);
+            // Lets say you store it as versionID in this case
+
+            // Download mod
+            //Requests.DownloadMod(versionID);
+            // returns links and mod types lets say stored as dldata.links and dldata.types
+            
+            // Download mod files
+            // Create loop for each item in dldata
+            //for (int i = 0; i < 1; i++)
+            //{
+                //define path
+
+                // if (dldata.types[i] == "mod") path = "/", if (dldata.types[i] == "pack") path = "/Custom Assets/Packs", etc.
+
+            //    Requests.DownloadFile(dldata.links[i],path);
+            //}
+
+            //Debug.Log("Mod installed");
+
+
 
             
         }
